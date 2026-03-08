f172e2ce6b495ef8ba8f8c57e1df0cd64629c54c
Adam Ahmed <109182351+doma990@users.noreply.github.com>
2026-03-08 03:12:47 +0200

    Add files via upload

---
+++ game/engine/cards/Card.java
@@ -0,0 +1,35 @@
+package game.engine.cards;
+
+public abstract class Card {
+	private String name;
+	private String description;
+	private int rarity;
+	private boolean lucky;
+	
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getRarity() {
+		return rarity;
+	}
+
+	public boolean isLucky() {
+		return lucky;
+	}
+
+	public Card(String name, String description, int rarity, boolean lucky) {
+		this.name = name;
+		this.description = description;
+		this.rarity = rarity;
+		this.lucky = lucky;
+	}
+	
+	public String toString() {
+		return name + ", " + description + ", " + rarity + ", " + lucky;
+	}
+}
