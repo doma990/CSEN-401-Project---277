dfe1cfb6ba1493b3e70974112b614072d34cd71e
Adam Ahmed <109182351+doma990@users.noreply.github.com>
2026-03-08 03:11:24 +0200

    Add files via upload

---
+++ game/engine/cells/Cell.java
@@ -0,0 +1,27 @@
+package game.engine.cells;
+
+import game.engine.monsters.*;
+
+public class Cell {
+	private String name;
+	private Monster monster;
+	
+	public Cell (String name) {
+		this.name = name;
+		monster = null;
+	}
+
+	public Monster getMonster() {
+		return monster;
+	}
+
+	public void setMonster(Monster monster) {
+		this.monster = monster;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+}
